3ce7734e97dd59971efa688d12ef62673742670c
DNS <karimovmayshod@gmail.com>
2020-12-06 23:30:24 +0300

    Add Praktika2

---
+++ src/ru/mirea/praktika2/TestAuthor.java
@@ -0,0 +1,9 @@
+package ru.mirea.praktika2;
+
+public class TestAuthor {
+
+    public static void main(String[] args) {
+        Author c1 = new Author("Mike", "fastscrul@gmail.com", 'm');
+        System.out.println(c1);
+    }
+}
---
+++ src/ru/mirea/praktika2/TestBall.java
@@ -0,0 +1,10 @@
+package ru.mirea.praktika2;
+
+
+public class TestBall {
+    public static void main(String[] args) {
+        Ball c1 = new Ball(15,3);
+        c1.move(3,5);
+        System.out.println(c1);
+    }
+}
