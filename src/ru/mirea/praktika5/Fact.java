f8119dab65b1f4ad60990a1902bd0afb16e124fd
DNS <karimovmayshod@gmail.com>
2020-12-08 04:21:53 +0300

    Add Praktika5

---
+++ src/ru/mirea/praktika5/Fact.java
@@ -0,0 +1,17 @@
+package ru.mirea.praktika5;
+
+
+public class Fact {
+
+    static int AAA(int N){
+        if(N == 0)
+            return 0;
+        if(N == 1)
+            return 1;
+        return N * AAA(N - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(AAA(5));
+    }
+}
---
+++ src/ru/mirea/praktika5/Posled.java
@@ -0,0 +1,20 @@
+package ru.mirea.praktika5;
+
+
+public class Posled{
+    public static void recursion() {
+        java.util.Scanner in = new java.util.Scanner(System.in);
+        System.out.println("Введите числа: ");
+        int n = in.nextInt();
+        if (n > 0) {
+            int m = in.nextInt();
+            System.out.println(n);
+            if (m > 0) {
+                recursion();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        recursion();
+    }
+}
