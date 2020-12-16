5295609187eec335d96fd56176e0329237a6fffd
DNS <karimovmayshod@gmail.com>
2020-12-16 06:17:52 +0300

    Add Praktika11

---
+++ src/ru/mirea/praktika11/Queue.java
@@ -0,0 +1,10 @@
+package ru.mirea.praktika11;
+
+public interface Queue {
+    boolean enqueue  (int n);
+    int element();
+    int dequeue ();
+    int size();
+    boolean isEmpty ();
+    void clear  ();
+}
