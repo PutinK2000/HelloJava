40d47e20903e338e1cf91a7f73821389a8b571d3
DNS <karimovmayshod@gmail.com>
2020-12-21 19:55:58 +0300

    Add Praktika16

---
+++ src/ru/mirea/praktika16/Item.java
@@ -0,0 +1,9 @@
+package ru.mirea.praktika16;
+
+
+public interface Item
+{
+    double getCost();
+    String getName();
+    String getDescription();
+}
---
+++ src/ru/mirea/praktika16/Order.java
@@ -0,0 +1,11 @@
+package ru.mirea.praktika16;
+
+
+public interface Order {
+    boolean add(Dish dish);
+    boolean add(Drink drink);
+    boolean remove(String name);
+    void removeAll(String name);
+    int getSize();
+    double CostTotal();
+}
